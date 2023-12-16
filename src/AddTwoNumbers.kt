8d4e71fe215220847b798df87e9ea05da5f4b669
manishkumar-byjus <100279960+manishkumar-byjus@users.noreply.github.com>
2023-12-16 20:32:22 +0530

    added Single Link List example 2

--- src/AddTwoNumbers.kt
+++ src/AddTwoNumbers.kt
@@ -32,3 +32,49 @@ fun addTwoNumbers(ln1: ListNode?, ln2: ListNode?): ListNode? {
     return dummy.next
 
 }
+
+fun addTwoNumbersV2(ln1: ListNode?, ln2: ListNode?): ListNode? {
+    var head1 = ln1
+    var head2 = ln2
+    // Head of the new linked list - this is the head of the resultant list
+    var head: ListNode? = null
+    // Reference of head which is null at this point
+    var temp: ListNode? = null
+    // Carry
+    var carry = 0
+    // Loop for the two lists
+    while (head1 != null || head2 != null) {
+        // At the start of each iteration, we should add carry from the last iteration
+        var sum = carry
+        // Since the lengths of the lists may be unequal, we are checking if the
+        // current node is null for one of the lists
+        if (head1 != null) {
+            sum += head1.`val`
+            head1 = head1.next
+        }
+        if (head2 != null) {
+            sum += head2.`val`
+            head2 = head2.next
+        }
+        // At this point, we will add the total sum % 10 to the new node
+        // in the resultant list
+        val node = ListNode(sum % 10)
+        // Carry to be added in the next iteration
+        carry = sum / 10
+        // If this is the first node or head
+        if (temp == null) {
+            head = node
+            temp = head
+        } else {
+            temp.next = node
+            temp = temp.next
+        }
+    }
+    // After the last iteration, we will check if there is carry left
+    // If it's left then we will create a new node and add it
+    if (carry > 0) {
+        temp!!.next = ListNode(carry)
+    }
+    return head
+
+}
